0cec84723627bb8037d0964ab33df2e1b247d9ee
TheFreakLord <freack1208@gmail.com>
2014-12-31 13:11:59 +0100

    removed some unneccessary lines.

--- PigPoop/src/me/freack100/pigpoop/PigPoop.java
+++ PigPoop/src/me/freack100/pigpoop/PigPoop.java
@@ -19,9 +19,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.*;
 
-/**
- * Created by Freack100.
- */
 public class PigPoop extends JavaPlugin implements Listener, CommandExecutor {
 
     private HashMap<Entity, Integer> cooldownList = new HashMap();
